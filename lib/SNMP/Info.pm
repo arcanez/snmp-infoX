5c07e9af931712a4a87ed6ca5c83a688bc265eff
arcanez <justin.d.hunter@gmail.com>
2009-06-07 13:08:57 -0700

    shell

---
+++ lib/SNMP/Info.pm
@@ -0,0 +1,9 @@
+package SNMP::Info;
+use Moose;
+use MooseX::Types::Moose qw(ArrayRef Str);
+
+has 'snmp_ver' => (is => 'rw', isa => Int, required => 0, default => 2);
+has 'snmp_comm' => (is => 'rw', isa => Str, required => 0, default => 'public');
+has 'snmp_user' => (is => 'rw', isa => Str, required => 0, default => 'initial');
+
+1;
