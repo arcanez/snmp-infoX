e10e8b6d591b8264f0bf2b1c0357205f04ba4509
arcanez <justin.d.hunter@gmail.com>
2009-06-07 13:09:56 -0700

    that'll teach me to copy code

--- lib/SNMP/Info.pm
+++ lib/SNMP/Info.pm
@@ -1,6 +1,6 @@
 package SNMP::Info;
 use Moose;
-use MooseX::Types::Moose qw(ArrayRef Str);
+use MooseX::Types::Moose qw(Int Str);
 
 has 'snmp_ver' => (is => 'rw', isa => Int, required => 0, default => 2);
 has 'snmp_comm' => (is => 'rw', isa => Str, required => 0, default => 'public');
