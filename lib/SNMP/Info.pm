a77e473e54182d2546ba92c2d0036bca13a94820
arcanez <justin.d.hunter@gmail.com>
2009-09-04 18:12:43 -0700

    remove required => 0 (redundant) add session object (holds SNMP::Session)

--- lib/SNMP/Info.pm
+++ lib/SNMP/Info.pm
@@ -2,8 +2,29 @@ package SNMP::Info;
 use Moose;
 use MooseX::Types::Moose qw(Int Str);
 
-has 'snmp_ver' => (is => 'rw', isa => Int, required => 0, default => 2);
-has 'snmp_comm' => (is => 'rw', isa => Str, required => 0, default => 'public');
-has 'snmp_user' => (is => 'rw', isa => Str, required => 0, default => 'initial');
+has 'snmp_ver' => (
+    is => 'rw',
+    isa => Int,
+    default => 2);
+
+has 'snmp_comm' => (
+    is => 'rw',
+    isa => Str,
+    default => 'public');
+
+has 'snmp_user' => (
+    is => 'rw',
+    isa => Str,
+    default => 'initial');
+
+has 'session' => (
+    is => 'rw',
+    isa => 'SNMP::Session',
+    lazy_build => 1,
+    handles => qr/.*/        
+);
+
+sub build_session {
+}
 
 1;
